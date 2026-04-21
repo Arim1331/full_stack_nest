4128695b1627fdaf7ea34d66bd1302c5189088ca
Arim1331 <wjsdkfla1331@gmail.com>
2026-04-21 21:02:31 +0900

    feat: 게시글 좋아요 JWT 연동 및 liked 상태 반환 로직 추가

--- src/controller/post/post.controller.ts
+++ src/controller/post/post.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { ApiOperation } from '@nestjs/swagger';
 import { PostCreateDTO, PostUpdatedDTO } from 'src/domain/post/dto/post.dto';
 import { PostService } from 'src/service/post/post.service';
+import type { AuthRequest } from 'src/type/auth.type';
 
 @Controller('posts') // /posts
 export class PostController {
@@ -10,22 +12,28 @@ export class PostController {
 
   // 게시글 전체 조회
   @ApiOperation({summary: "게시글 전체 조회"})
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(200)
-  @Get("") 
-  async getPosts() {
-    return await this.postService.getPosts()
+  @Get() 
+  async getPosts(@Req() req: AuthRequest) {
+    const memberId = req.user?.id
+    return await this.postService.getPosts(memberId)
   }
 
   // 게시글 단일 조회
   @ApiOperation({summary: "게시글 단일 조회"})
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(200)
   @Get(":id") // /posts/:id
-  async getPost(@Param("id") id: string) {
-    return await this.postService.getPost(Number(id))
+  async getPost(@Param("id") id: string, @Req() req: AuthRequest) {
+    const memberId = req.user?.id
+
+    return await this.postService.getPost(Number(id), memberId)
   }
 
   // 게시글 생성
   @ApiOperation({summary: "게시글 생성"})
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(201)
   @Post("")
   async create(@Body() postCreateDTO: PostCreateDTO) {
@@ -34,6 +42,7 @@ export class PostController {
 
   // 게시글 수정
   @ApiOperation({summary: "게시글 수정"})
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(200)
   @Put(":id") 
   async update(@Param("id") id: string, @Body() postUpdatedDTO: PostUpdatedDTO) {
@@ -42,6 +51,7 @@ export class PostController {
 
   // 게시글 삭제
   @ApiOperation({summary: "게시글 삭제"})
+  @UseGuards(AuthGuard('jwt'))
   @HttpCode(200)
   @Delete(":id")
   async remove(@Param("id") id: string) {
